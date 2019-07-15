f2002629111fef112bf092e8186d970109333b6d
unknown <unknown.guy12352@gmail.com>
2019-07-15 10:13:31 +0545

    first commit

---
+++ js/galleryEvent.js
@@ -0,0 +1,181 @@
+// // var photo = document.getElementById("1");
+
+// var photo = document.querySelectorAll('.photos img');
+// // console.log(photo);
+
+// var placeholder = document.getElementById('2');
+// var photos = ['portfolio1.jpg', 'portfolio2.jpg', 'portfolio3.png', 'portfolio4.jpeg', 'portfolio5.jpeg', 'portfolio6.jpeg', 'portfolio7.jpeg', 'portfolio8.jpg'];
+
+// for (var i = 0; i < photo.length; i++) {
+
+//   // console.log(photo[i]);
+//   // photo[i].style.width = '219px';
+//   // photo[i].style.height = '159px';
+//   photo[i].src = "./img/gallery/" + photos[i] + " ";
+//   //photo[i].src = './img/gallery/ace.jpg ';
+
+//   photo[i].onclick = function () {
+
+//     console.log(photos);
+//     placeholder.src = "./img/gallery/" + photos[0] + " ";
+
+//     console.log(placeholder)
+
+//   }
+
+// }
+
+
+// placeholder.style.width = '462px';
+// placeholder.style.height = '345px';
+
+// photo.onclick = function () {
+
+//   placeholder.src = './img/gallery/ace.jpg ';
+
+//   console.log(placeholder)
+
+// }
+
+
+
+// placeholder.onmouseenter = function () {
+
+//   console.log('hello');
+
+// };
+
+
+
+var pholder = document.querySelectorAll('.photos .hover');
+var dsply = document.querySelectorAll('.photos .main-image');
+
+
+// for (var i = 0; i < dsply.length; i++) {
+
+
+//   dsply[i].onmouseenter = function () {
+
+//     pholder[i].style.display = "block";
+
+//   }
+
+
+//   pholder[i].onmouseout = function () {
+
+//     pholder[i].style.display = "none";
+
+//   }
+
+
+// }
+
+
+
+
+dsply[0].onmouseenter = function () {
+
+  pholder[0].style.display = "block";
+
+}
+
+
+pholder[0].onmouseout = function () {
+
+  pholder[0].style.display = "none";
+
+}
+
+dsply[1].onmouseenter = function () {
+
+  pholder[1].style.display = "block";
+
+}
+
+
+pholder[1].onmouseout = function () {
+
+  pholder[1].style.display = "none";
+
+}
+
+
+dsply[2].onmouseenter = function () {
+
+  pholder[2].style.display = "block";
+
+}
+
+
+pholder[2].onmouseout = function () {
+
+  pholder[2].style.display = "none";
+
+}
+
+
+dsply[3].onmouseenter = function () {
+
+  pholder[3].style.display = "block";
+
+}
+
+
+pholder[3].onmouseout = function () {
+
+  pholder[3].style.display = "none";
+
+}
+
+dsply[4].onmouseenter = function () {
+
+  pholder[4].style.display = "block";
+
+}
+
+
+pholder[4].onmouseout = function () {
+
+  pholder[4].style.display = "none";
+
+}
+
+dsply[5].onmouseenter = function () {
+
+  pholder[5].style.display = "block";
+
+}
+
+
+pholder[5].onmouseout = function () {
+
+  pholder[5].style.display = "none";
+
+}
+
+dsply[6].onmouseenter = function () {
+
+  pholder[6].style.display = "block";
+
+}
+
+
+pholder[6].onmouseout = function () {
+
+  pholder[6].style.display = "none";
+
+}
+
+dsply[7].onmouseenter = function () {
+
+  pholder[7].style.display = "block";
+
+}
+
+
+pholder[7].onmouseout = function () {
+
+  pholder[7].style.display = "none";
+
+}
+
---
+++ js/iconHover.js
@@ -0,0 +1,51 @@
+function toggle() {
+
+  var primaryMenu = document.getElementById('primary-menu');
+
+  if (primaryMenu.style.display == 'block') {
+    primaryMenu.style.display = 'none';
+  } else {
+    primaryMenu.style.display = 'block';
+  }
+
+}
+
+function changeImageFirst() {
+  var mainImage = document.getElementById('main-image');
+  mainImage.src = "./img/gallery/portfolio1.png";
+}
+
+function changeImageSecond() {
+  var mainImage = document.getElementById('main-image');
+  mainImage.src = "./img/gallery/portfolio2.png";
+}
+
+function changeImageThree() {
+  var mainImage = document.getElementById('main-image');
+  mainImage.src = "./img/gallery/portfolio3.png";
+}
+
+function changeImageFour() {
+  var mainImage = document.getElementById('main-image');
+  mainImage.src = "./img/gallery/portfolio4.png";
+}
+
+function changeImageFive() {
+  var mainImage = document.getElementById('main-image');
+  mainImage.src = "./img/gallery/portfolio5.png";
+}
+
+function changeImageSix() {
+  var mainImage = document.getElementById('main-image');
+  mainImage.src = "./img/gallery/portfolio6.png";
+}
+
+function changeImageSeven() {
+  var mainImage = document.getElementById('main-image');
+  mainImage.src = "./img/gallery/portfolio7.png";
+}
+
+function changeImageEight() {
+  var mainImage = document.getElementById('main-image');
+  mainImage.src = "./img/gallery/portfolio8.png";
+}
